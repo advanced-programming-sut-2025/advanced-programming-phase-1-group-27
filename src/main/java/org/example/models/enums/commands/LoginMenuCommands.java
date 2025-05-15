9318d496c015f693356d8740c4dae7e2c8a20db5
sobhan behzadipour <sobhan.behzadipour@gmail.com>
2025-05-15 17:13:44 +0330

    رسا ترک خر

--- src/main/java/org/example/models/enums/commands/LoginMenuCommands.java
+++ src/main/java/org/example/models/enums/commands/LoginMenuCommands.java
@@ -10,7 +10,7 @@ public enum LoginMenuCommands implements MenuCommands {
             "-e\\s+(?<email>.+)\\s+" +
             "-g\\s+(?<gender>.+)"),
     PickQuestion("pick\\s+question\\s+-q\\s+(?<questionId>-?\\d+)\\s+-a\\s+(?<answer>.+)\\s+-c\\s+(?<reenteredAnswer>.+)"),
-    Login("login\\s+-u\\s+(?<username>.+)logout\\s+-p\\s+(?<password>.+)(?:\\s+(?<stay>--stay-logged-in))?"),
+    Login("login\\s+-u\\s+(?<username>.+)\\s+-p\\s+(?<password>.+)(?:\\s+(?<stay>--stay-logged-in))?"),
     ForgetPassword("forget\\s+password\\s+-u\\s+(?<username>.+)"),
     AnswerQuestion("answer\\s+-a\\s+(?<answer>.+)"),
     EnterMenu("menu\\s+enter\\s+(?<menuName>.+)"),
