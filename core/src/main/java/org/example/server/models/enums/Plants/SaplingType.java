8c8c39f8c8aa946f99c7fa3306eb9c29d533ceb8
amiryusofabdi <amiryusofabdiabdi1@gmail.com>
2025-07-24 11:57:24 +0330

    Items added

--- core/src/main/java/org/example/server/models/enums/Plants/SaplingType.java
+++ core/src/main/java/org/example/server/models/enums/Plants/SaplingType.java
@@ -3,21 +3,23 @@ package org.example.server.models.enums.Plants;
 import org.example.server.models.Item;
 
 public enum SaplingType implements Item, PlantSourceType {
-    ApricotSapling(TreeType.ApricotTree),
-    CherrySapling(TreeType.CherryTree),
-    BananaSapling(TreeType.BananaTree),
-    MangoSapling(TreeType.MangoTree),
-    OrangeSapling(TreeType.OrangeTree),
-    PeachSapling(TreeType.PeachTree),
-    AppleSapling(TreeType.AppleTree),
-    PomegranateSapling(TreeType.PomegranateTree),
-    Acorn(TreeType.OakTree),
-    PineCone(TreeType.PineTree);
+    ApricotSapling(TreeType.ApricotTree,"Items/SaplingProduct/Apricot_Sapling.png"),
+    CherrySapling(TreeType.CherryTree,"Items/SaplingProduct/Cherry_Sapling.png"),
+    BananaSapling(TreeType.BananaTree,"Items/SaplingProduct/Banana_Sapling.png"),
+    MangoSapling(TreeType.MangoTree,"Items/SaplingProduct/Mango_Sapling.png"),
+    OrangeSapling(TreeType.OrangeTree,"Items/SaplingProduct/Orange_Sapling.png"),
+    PeachSapling(TreeType.PeachTree,"Items/SaplingProduct/Peach_Sapling.png"),
+    AppleSapling(TreeType.AppleTree,"Items/SaplingProduct/Apple_Sapling.png"),
+    PomegranateSapling(TreeType.PomegranateTree,"Items/SaplingProduct/Pomegranate_Sapling.png"),
+    Acorn(TreeType.OakTree,"Items/SaplingProduct/Acorn.png"),
+    PineCone(TreeType.PineTree,"Items/SaplingProduct/Pine_Cone.png"),;
 
     private final TreeType tree;
+    private final String address;
 
-    SaplingType(TreeType plantType) {
+    SaplingType(TreeType plantType, String address) {
         this.tree = plantType;
+        this.address = address;
     }
 
     public static SaplingType getItem(String itemName) {
