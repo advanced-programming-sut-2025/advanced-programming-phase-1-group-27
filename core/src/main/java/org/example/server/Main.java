7df2f952a6317ee01926461a24dbffd918daaae3
rassa <rassa.mohammadi@gmail.com>
2025-07-26 14:17:08 +0330

    ServerLauncher using libgdx

--- core/src/main/java/org/example/server/Main.java
+++ core/src/main/java/org/example/server/Main.java
@@ -2,6 +2,7 @@ package org.example.server;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import org.example.common.database.DataBaseHelper;
 import org.example.server.models.ServerApp;
@@ -27,36 +28,19 @@ public class Main extends Game {
             ServerApp.setListenerThread(new ListenerThread(port));
             ServerApp.startListening();
 
-            // Start CLI thread
-            new Thread(this::startCLI).start();
-
         } catch (Exception e) {
             System.err.println("Error starting server: " + e.getMessage());
             Gdx.app.exit();
         }
     }
 
-    /**
-     * CLI Command Processor (Runs in a separate thread)
-     */
-    private void startCLI() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Server CLI started. Type 'help' for commands.");
-
-        while (!ServerApp.hasEnded()) {
-            // cli commands
-        }
-        scanner.close();
-    }
-
-    // ... (Rest of your existing methods: getMain(), getBatch(), etc.)
-
     @Override
     public void create() {
         main = this;
         batch = new SpriteBatch();
-        Gdx.app.exit();
-        // No need to call Gdx.app.exit() here since we handle it via CLI
+        while (!ServerApp.hasEnded()) {
+            // maybe cli commands
+        }
     }
 
     @Override
